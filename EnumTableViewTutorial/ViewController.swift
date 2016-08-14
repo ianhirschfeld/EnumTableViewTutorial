edb8ba5257821791aa037d6e0bb35609e4d45c81
Ian Hirschfeld <ianh7976@gmail.com>
2016-08-13 18:16:43 -0700

    Initial Commit

---
+++ EnumTableViewTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EnumTableViewTutorial
+//
+//  Created by Ian Hirschfeld on 8/13/16.
+//  Copyright © 2016 Ian Hirschfeld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
